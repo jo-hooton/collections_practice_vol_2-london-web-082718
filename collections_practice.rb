22fe47001d0b692546195837b88c900c9232a64e
Jo Hooton <joanneelizabethhooton@gmail.com>
2018-08-27 19:29:21 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,33 @@
-# your code goes here
+def begins_with_r(array)
+  array.all? { |str| str.start_with?('r')}
+end
+
+def contain_a(array)
+  array.select { |str| str.include?('a')}
+end
+
+def first_wa(array)
+  array.find { |str| str.start_with?('w')}
+end
+
+def remove_non_strings(array)
+  array.each { |str| (str.is_a? String) == true }
+end
+
+def count_elements(array)
+
+end
+
+def merge_data
+
+end
+
+def find_cool(array)
+  array.find do |str|
+    str == "cool"
+  end
+end
+
+def organize_schools(array)
+  array.sort
+end
